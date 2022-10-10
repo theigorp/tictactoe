6dc3abf1fd81e77fa9c074547c560d2fd60c2763
Igor <mr.igor187@gmail.com>
2022-10-10 05:18:15 +0200

    change .start to #start to match other selectors, initiate DOM variables in JS

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const Gameboard = (() => {
     const grid = document.querySelector('.game');
+    const buttonX  = document.querySelector('#player-x');
+    const buttonO  = document.querySelector('#player-o');
+    const startButton = document.querySelector('#start');
 })();
