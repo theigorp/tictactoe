ebceb21f641ab24a95948b1602fafc4fb8d6b073
Igor <mr.igor187@gmail.com>
2022-10-10 05:33:30 +0200

    define eventListener for determining playerChoice value

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ const Gameboard = (() => {
     const buttonX  = document.querySelector('#player-x');
     const buttonO  = document.querySelector('#player-o');
     const startButton = document.querySelector('#start');
+    const startControlsEl = document.querySelector('.start-controls');
+    let playerChoice;
+
+    startControlsEl.addEventListener('click', (e) => {
+        if(e.target.id == 'player-x')
+        {
+            console.log('Player choose X');
+            playerChoice = 'X';
+        }
+        else if(e.target.id == 'player-o')
+        {
+            console.log('Player choose O');
+            playerChoice = 'O';
+        }
+        else if(e.target.id == 'start')
+        {
+            //funkcija koja ce da hoveruje x ili o oko cursora
+            startControlsEl.style.display = 'none';
+        }
+    });
+    
 })();
